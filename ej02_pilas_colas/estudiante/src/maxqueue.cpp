57705aab9f11fd9a76f5ee0041bd9f0e0c1d6dcd
alonsodm12 <alonsodmx@gmail.com>
2022-10-24 16:53:54 +0200

    prueba

--- ej02_pilas_colas/estudiante/src/maxqueue.cpp
+++ ej02_pilas_colas/estudiante/src/maxqueue.cpp
@@ -1,5 +1,58 @@
 /**
  * @file maxqueue.cpp
  * @brief  Archivo de implementación del TDA MaxQueue
- * @author
+ * @author 
  */
+
+
+
+#include "maxqueue.h"
+
+void Cola::push(int numero){
+    element nuevo_num;
+    nuevo_num.value=numero;
+
+    if(!datos.empty() && datos.front().max_value > numero)
+        nuevo_num.max_value=datos.front().max_value;
+
+    else
+        nuevo_num.max_value=numero;
+
+    //una vez que ya tenemos hechas las comprobaciones de 
+    //que sea el mayor numero de los dos creamos un vector
+    //auxiliar y lo rellenamos
+
+    queue<element> datos2;
+    
+    //Introducimos el dato que hemos creado con 
+    //los valores pasados como argumento
+    
+    datos2.push(nuevo_num);
+
+    while(!datos.empty()){
+        datos2.push(datos.front());
+        datos.pop();
+
+    }
+
+    datos=datos2;
+    //
+
+}
+
+void Cola::pop(){
+
+    datos.pop();
+
+}
+
+int Cola::size(){
+    return datos.size();
+}
+
+element Cola::front(){
+    return datos.front();
+}
+bool Cola::empty() const{
+    return datos.empty();
+}
